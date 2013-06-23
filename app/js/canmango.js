47cc8e08bcd0cd80c3fed4ab43afc356577ab6ff
Chris Benwar <chris@not.email>
2013-06-23 23:37:58 +0100

    Build face designer using basic canvas ovals and squares.

--- app/js/canmango.js
+++ app/js/canmango.js
@@ -20,91 +20,226 @@ var canmango = canmango || {};
 			my.width = my.stage.canvas.width;
 			my.height = my.stage.canvas.height;
 
-			var shape = new createjs.Shape();
+			my.draw();
+		},
+		draw: function() {
+			var my = cm.shapespasm;
+
+			my.stage.removeAllChildren();
+
+			var container = new createjs.Container();
+			var w = 300;
+			var h = 300;
 
 			if(my.drawer) {
-				my.drawer.draw(shape, my.width / 10, my.height / 10);
+				my.drawer.draw(container, w, h);
 			}
 
-			my.stage.addChild(shape);
+			container.regX = w / 2;
+			container.regY = h / 2;
+			container.x = my.width / 2;
+			container.y = my.height / 2;
+
+			my.stage.addChild(container);
 			my.stage.update();
-		},
+			
+		}
 	},	
 
 	cm.roundDude = {
 		"dude": {
+			"atoms": {
+				"head": { "w": 1, "h": 1, "colour1": "brown", "colour2": "black", "shape": "square" },
+				"eye": { "w": 0.1, "h": 0.2, "colour1": "red", "colour2": "black" },
+				"pupil": { "w": 0.1, "h": 0.1, "colour1": "black", "colour2": "white", "shape": "square" },
+				"mouth": { "w": 0.1, "h": 0.4, "colour1": "white", "colour2": "pink", "shape": "square" },
+				"foot": { "w": 0.4, "h": 0.2, "colour1": "brown", "colour2": "black", "shape": "circle" },
+			},
 			"parts": {
-				"head": { "w": 1, "h": 1, "colour1": "brown", "colour2": "black" },
-				"eye": { "w": 0.4, "h": 0.2, "colour1": "white", "colour2": "black" },
-				"pupil": { "w": 0.05, "h": 0.05, "colour1": "black", "colour2": "black" },
-				"mouth": { "w": 0.6, "h": 0.1, "colour1": "yellow", "colour2": "black" },
-				"foot": { "w": 0.4, "h": 0.2, "colour1": "black", "colour2": "black" },
+				"dude": {"x": 0, "y": 0, "abs": 1},
+				"head": { "atom": "head", "parent": "dude", "abs": 1 },
+				"eyes": {"parent": "head", "x": 0.5, "y": 0.3 },
+				"eye1": { "atom": "eye", "parent": "eyes", "x": -0.2 },
+				"eye2": { "atom": "eye", "parent": "eyes", "x": 0.2 },
+				"pupil1": { "atom": "pupil", "parent": "eye1", "x": 0.05, "y": 0.1 },
+				"pupil2": { "atom": "pupil", "parent": "eye2", "x": 0.05, "y": 0.1 },
+				"mouth": { "atom": "mouth", "parent": "head", "x": 0.5, "y": 0.7 },
+				"feet": { "parent": "dude", "x": 0.5, "y": 1 },
+				"foot1": { "atom": "foot", "parent": "feet", "x": -0.25 },
+				"foot2": { "atom": "foot", "parent": "feet", "x": 0.25 }
 			},
-			"families": {
-				"head": { "part": "head" },
-				"eye1": { "part": "eye", "parent": "head" },
-				"eye2": { "part": "eye", "parent": "head" },
-				"pupil1": { "part": "pupil", "parent": "eye1" },
-				"pupil2": { "part": "pupil", "parent": "eye2" },
-				"mouth": { "part": "mouth", "parent": "head" },
-				"foot1": { "part": "foot", "parent": "head" },
-				"foot2": { "part": "foot", "parent": "head" }
-			},
-			"layouts": {
-				"head": { "parts": ["head"] , "y": 0},
-				"eyes": { "parts": ["eye1", "eye2"], "d": 0.5, "y": 0.3 },
-				"pupils": { "parts": ["pupil1", "pupil2"], "x": 0.1, "y": 0.1 },
-				"mouth": { "parts": ["mouth"], "y": 0.7 },
-				"feet": { "parts": ["foot1", "foot2"], "d": 0.5, "y": 0.9 }
-			}
+			"order": [
+				"dude", "head", "eyes", "eye1", "eye2", "pupil1", "pupil2",
+				"mouth", "feet", "foot1", "foot2"
+			]
 		},
-		"cache": { },
-		draw: function(shape, w, h) {
-			var g = shape.graphics;
+		"containers": {},
+		"displayObjects": { },
+		redraw: function() {
+			var my = canmango.roundDude;
+			my.containers = {};
+			my.displayObjects = {};
+			canmango.shapespasm.draw();
+		},
+		getDisplayData: function() {
+			var my = canmango.roundDude;
+			var dude = my.dude;
+			var atomID = null, atom = null;
+			var displayData = [];
+			var desc = {
+				"head": "Head", "eye": "Eyes", "pupil": "Pupils",
+				"mouth": "Mouth", "foot": "Feet"
+			};
+
+			for(atomID in dude.atoms) {
+				atom = dude.atoms[atomID];
+
+				displayData.push({
+					"atomID": atomID, 
+					"desc": desc[atomID],	
+					"w": atom.w,
+					"h": atom.h,
+					"colour1": atom.colour1,
+					"colour2": atom.colour2,
+					"shape": atom.shape
+				});
+			}
+
+			return displayData;
+		},
+		update: function(displayData) {
+			var my = canmango.roundDude;
+			var part = null;
+			var dude = my.dude;
+			var atomID = displayData.atomID;
+
+			dude.atoms[atomID].colour1 = displayData.colour1;
+			dude.atoms[atomID].colour2 = displayData.colour2;
+			dude.atoms[atomID].shape = displayData.shape;
+			dude.atoms[atomID].w = displayData.w;
+			dude.atoms[atomID].h = displayData.h;
+
+			my.redraw();
+		},
+		draw: function(stage, w, h) {
 			var my = canmango.roundDude;
 			var parts = my.dude.parts;
-			var layouts = my.dude.layouts;
-			var families = my.dude.families;
+			var atoms = my.dude.atoms;
+			var order = my.dude.order;
 			var partInfo = null, part = null, layout = null, family = null;
-			var atomID = null, partID = null;
+			var atomID = null, partID = null, atom = null;
 			var x = 0, y = 0, top = 0, height = 0, d = 0, partX = 0, partY = 0;
-			var parentPos = null, parentPartID = null;
-			
-			for(var layoutID in layouts) {
-				layout = layouts[layoutID];
-				x = layout.x || 0;
-				y = layout.y || 0;
-				d = layout.d || 0;
+			var parentPart = null, parentPartID = null, parentDO = null;
+			var partShape = null, partG = null, displayObj = null;
+			var container = null;
 
-				for(var i = 0; i < layout.parts.length; i++)
+			for(partID in parts) {
+				part = parts[partID];
+				if(part.parent)
 				{
-					partX = x;
-					partY = y;
+					container = new createjs.Container();
+					my.containers[part.parent] = container;
+				}
+			}
 
-					partX += d * i;
-					partID = layout.parts[i];
-					atomID = families[partID].part;
-					part = parts[atomID];
+			for(var i = 0; i < order.length; i++)
+			{
+				partID = order[i];
+				part = parts[partID];	
 
-					parentPartID = families[partID].parent;
-					parentPos = my.cache[parentPartID];
-					if(parentPos)
+			}
+
+			for(var i = 0; i < order.length; i++)
+			{
+				partID = order[i];
+
+				part = parts[partID];	
+				x = part.x || 0;
+				y = part.y || 0;
+
+				if(my.containers[partID])
+				{
+					container = my.containers[partID];
+
+					if(part.parent)
 					{
-						partX += parentPos.x;
-						partY += parentPos.y;
+						my.containers[part.parent].addChild(container);
+					}
+					else
+					{
+						stage.addChild(container);
+					}
+				}
+
+				if(my.containers[partID])
+				{
+					parentDO = my.containers[partID];	
+				}
+				else if(part.parent)
+				{
+					parentPart = parts[part.parent];
+					parentDO = my.containers[part.parent];
+				}
+
+				if(my.containers[partID] && !part.atom)
+				{
+					my.containers[partID].x = x * w;
+					my.containers[partID].y = y * w;
+				}
+
+				if(part.atom)
+				{
+					atom = atoms[part.atom];
+					partG = new createjs.Graphics().beginFill(atom.colour1);	
+					partG.setStrokeStyle(4);
+					partG.beginStroke(atom.colour2);	
+
+					var shape = atom.shape || 'ellipse';
+
+					if(my.containers[partID])
+					{
+						atom = atoms[part.atom];
+						if(!part.abs)
+						{
+							my.containers[partID].regX = atom.w * w / 2;
+							my.containers[partID].regY = atom.h * h / 2;
+						}
+
+						my.containers[partID].x = x * w;
+						my.containers[partID].y = y * w;
+						my.drawShape(partG, shape, atom.w * w, atom.h * h);
+						displayObj = new createjs.Shape(partG);
+					}
+					else
+					{
+						my.drawShape(partG, shape, atom.w * w, atom.h * h);
+
+						displayObj = new createjs.Shape(partG);
+						displayObj.regX = atom.w * w / 2;
+						displayObj.regY = atom.h * h / 2;
+						displayObj.x = x * w;
+						displayObj.y = y * h;
 					}
 
-					my.cache[partID] = { "x": partX, "y": partY };
-					
-					my.drawPart(g, part, partX, partY, w, h);
+					my.displayObjects[partID] = displayObj;
+					parentDO.addChild(displayObj);
 				}
 			}
 		},
-		drawPart: function(g, part, x, y, w, h) {
-				var height = h * part.h;
+		drawShape: function(g, type, w, h) {
+			var radius = w;
+			if(h > w) radius = h;
 
-				g.beginFill(part.colour1);
-				g.drawEllipse(x * w, y * h, w * part.w, h * part.h);
+			switch(type)
+			{
+				case 'square':
+					g.drawRect(0, 0, w, h);
+					break;
+				case 'ellipse':
+				default:
+					g.drawEllipse(0, 0, w, h);
+					break;
+			}
 		},
 		drawParts: function(shape, w, h) {
 			var g = shape.graphics;
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,20 @@ var canmango = canmango || {};
 
 (function(cm) {
 	angular.module('canmango.controllers', []).
+	
 		controller('HomeCtrl', function($scope) {
-			var drawer = canmango.roundDude;
-			canmango.shapespasm.setDrawer(drawer);
+
+			var drawer = cm.roundDude;
+			cm.shapespasm.setDrawer(drawer);
+
 			$scope.$on('$viewContentLoaded', canmango.shapespasm.init);
+
+			var displayData = cm.roundDude.getDisplayData();
+			$scope.displayData = displayData;
+			$scope.selectedItem = displayData[0];
+
+			$scope.update = function() {
+				cm.roundDude.update($scope.selectedItem);
+			}
   });
 })(canmango);
